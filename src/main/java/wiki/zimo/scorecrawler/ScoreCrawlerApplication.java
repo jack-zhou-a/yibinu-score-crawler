303a18d1f89e4f255774a34dae13bbbb3ee9ddd7
子墨 <461009747@qq.com>
2020-03-01 17:56:31 +0800

    package ready

--- src/main/java/wiki/zimo/scorecrawler/ScoreCrawlerApplication.java
+++ src/main/java/wiki/zimo/scorecrawler/ScoreCrawlerApplication.java
@@ -2,12 +2,26 @@ package wiki.zimo.scorecrawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ScoreCrawlerApplication {
+public class ScoreCrawlerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ScoreCrawlerApplication.class, args);
     }
 
+    /**
+     * @auther: 子墨
+     * @date: 2018/11/24 19:24
+     * @describe: Tomcat启动Springboot项目
+     * @param: [builder]
+     * @return: org.springframework.boot.builder.SpringApplicationBuilder
+     * @version v1.0
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
