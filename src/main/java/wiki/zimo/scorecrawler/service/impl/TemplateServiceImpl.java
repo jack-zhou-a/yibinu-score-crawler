6da0b133d39ee21f7868f85219320934df88f9e9
子墨 <461009747@qq.com>
2020-03-02 21:42:49 +0800

    成绩单导å生成模块完成

--- src/main/java/wiki/zimo/scorecrawler/service/impl/TemplateServiceImpl.java
+++ src/main/java/wiki/zimo/scorecrawler/service/impl/TemplateServiceImpl.java
@@ -1,5 +1,6 @@
 package wiki.zimo.scorecrawler.service.impl;
 
+import com.deepoove.poi.XWPFTemplate;
 import org.apache.poi.xwpf.usermodel.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
@@ -21,104 +22,100 @@ public class TemplateServiceImpl implements TemplateService {
     @Override
     public void rendering(Student student) throws IOException {
         File templateFile = ResourceUtils.getFile("classpath:template.docx");
-        System.out.println(templateFile.getAbsolutePath());
+//        System.out.println(templateFile.getAbsolutePath());
+
+        // 模板填充docx对象
+        XWPFTemplate template = XWPFTemplate.compile(templateFile);
 
         // 准备塞入模板中的文本信息
-        Map<String, Object> textMap = new HashMap<>();
-        textMap.put("xh", student.getXh());
-        textMap.put("xm", student.getXm());
-        textMap.put("bj", student.getBj());
-        textMap.put("nj", student.getNj());
-        textMap.put("yxmc", student.getYxmc());
-        textMap.put("zymc", student.getZymc());
-        textMap.put("xz", student.getXz());
-        textMap.put("qdxf", student.getQdxf());
-        textMap.put("pjjd", student.getPjjd());
+        Map<String, Object> model = new HashMap<>();
+        model.put("xh", student.getXh());
+        model.put("xm", student.getXm());
+        model.put("bj", student.getBj());
+        model.put("nj", student.getNj());
+        model.put("yxmc", student.getYxmc());
+        model.put("zymc", student.getZymc());
+        model.put("xz", student.getXz());
+        model.put("qdxf", student.getQdxf());
+        model.put("pjjd", student.getPjjd());
 
         Calendar now = Calendar.getInstance();
-        textMap.put("year", now.get(Calendar.YEAR));
-        textMap.put("month", now.get(Calendar.MONTH) + 1);
-        textMap.put("day", now.get(Calendar.DAY_OF_MONTH));
+        model.put("year", now.get(Calendar.YEAR));
+        model.put("month", now.get(Calendar.MONTH) + 1);
+        model.put("day", now.get(Calendar.DAY_OF_MONTH));
 
-        // 读取文档模板
-        XWPFDocument doc = new XWPFDocument(new FileInputStream(templateFile));
-
-        // 获取段落并匹配修改变量
-        List<XWPFParagraph> paragraphs = doc.getParagraphs();
-        for (XWPFParagraph p : paragraphs) {
-            List<XWPFRun> runs = p.getRuns();
-            for (XWPFRun r : runs) {
-                String s = r.toString();
-                if (s != null && s.startsWith("{{") && s.endsWith("}}")) {
-                    String key = s.substring(2, s.length() - 2);
-                    r.setText(textMap.get(key).toString(), 0);
-                }
-            }
-        }
-
-        // 准备动态填入表格的成绩信息
+        // 构造塞入模板的成绩信息，模板中表格固定是36行，21列，分为三个区域，每7列一个区域
         List<Score> scores = student.getScores();
+        final int rows = 36;
+        final int area = 7;
+        int n = scores.size() / (rows - 1) + 1;// 需要循环写几次
+        int pos = 0;// 标记当前正在填入的score数据集下标
+        int time = 1;// 标记第几次填区域
+        label:
+        while (n-- > 0) {
+            int startCol = 0;
+            switch (time) {
+                case 1:
+                    startCol = 0;
+                    break;
+                case 2:
+                    startCol = 7;
+                    break;
+                case 3:
+                    startCol = 14;
+                    break;
+            }
 
-        // 获取表格并动态修改内容，模板中表格固定是35行，21列，分为三个区域，每7列一个区域
-        XWPFTable table = doc.getTables().get(0);
-        List<XWPFTableRow> rows = table.getRows();
-
-        int n = scores.size() / (rows.size() - 1) + 1;// 需要循环写几次
-        int pos = 0;
-        l:
-        for (int index = 0; index < n; index++) {
-//            System.out.println(index);
-            // 0行是表头，内容应该从1开始
-            for (int i = 1; i < rows.size(); i++) {
-                XWPFTableRow row = rows.get(i);
-                List<XWPFTableCell> cells = row.getTableCells();
-                // 每次数据只写7列
-                for (int j = index * 7; j < index * 7 + 7; j++) {
-                    XWPFTableCell cell = cells.get(j);
-                    List<XWPFParagraph> ps = cell.getParagraphs();
-                    for (XWPFParagraph p : ps) {
-                        List<XWPFRun> runs = p.getRuns();
-                        for (XWPFRun r : runs) {
-                            switch (j - index * 7) {
-                                case 0:
-                                    r.setText(String.valueOf(scores.get(pos).getXqSort()), 0);
-                                    break;
-                                case 1:
-                                    r.setText(scores.get(pos).getKcmc(), 0);
-                                    break;
-                                case 2:
-                                    r.setText(scores.get(pos).getKclb(), 0);
-                                    break;
-                                case 3:
-                                    r.setText(scores.get(pos).getXdzk(), 0);
-                                    break;
-                                case 4:
-                                    r.setText(String.valueOf(scores.get(pos).getXf()), 0);
-                                    break;
-                                case 5:
-                                    r.setText(String.valueOf(scores.get(pos).getCj()), 0);
-                                    break;
-                                case 6:
-                                    r.setText(String.valueOf(scores.get(pos).getJd()), 0);
-                                    break;
-                            }
-
-                            pos++;
-
-                            // 写完了，退出
-                            if (pos >= scores.size()) {
-                                break l;
-                            }
-                        }
+//            System.out.println("time=" + time + ",startCol=" + startCol);
+            for (int i = 1; i < rows; i++) {
+                for (int j = startCol; j < area + startCol; j++) {
+                    String key = "r" + i + "c" + j;
+                    String val = null;
+                    switch (j - startCol) {
+                        case 0:
+                            val = String.valueOf(scores.get(pos).getXqSort());
+                            break;
+                        case 1:
+                            val = scores.get(pos).getKcmc();
+                            break;
+                        case 2:
+                            val = scores.get(pos).getKclb();
+                            break;
+                        case 3:
+                            val = scores.get(pos).getXdzk();
+                            break;
+                        case 4:
+                            val = String.valueOf(scores.get(pos).getXf());
+                            break;
+                        case 5:
+                            val = String.valueOf(scores.get(pos).getCj());
+                            break;
+                        case 6:
+                            val = String.valueOf(scores.get(pos).getJd());
+                            break;
                     }
+                    model.put(key, val);
+//                    System.out.println(key + "," + val);
+                }
+
+                pos++;
+                // 所有信息已经填完结束了
+                if (pos >= scores.size()) {
+                    break label;
                 }
             }
+            time++;
         }
+//        System.out.println(n);
 
-        System.out.println("pos=" + pos);
+        // 塞入模板文档
+        template.render(model);
+//        System.out.println(model);
+
+//        System.out.println("pos=" + pos);
 
         FileOutputStream out = new FileOutputStream(new File("C:\\Users\\zimo\\Desktop\\out.docx"));
-        doc.write(out);
+        template.write(out);
 
         out.close();
     }
